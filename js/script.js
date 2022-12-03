948184a1b4a016bb8ca567a9de4468092c765f27
BeginnerFE <112899153+BeginnerFE@users.noreply.github.com>
2022-12-04 00:22:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log('Я вчу');
+console.log('JS');
+console.log('Я вчу' + ' JS');
+
+
+let usersColorEyes = 'brown';
+console.log(usersColorEyes);
+
+let userName = 'Вася';
+let user;
+user = userName;
+console.log(user);
